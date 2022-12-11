4e5c356c2227fdfae11224ca663acfa691ac9d54
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-12-11 16:23:24 +0200

    add mui dependencies, configure font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,10 @@
 import './App.css';
-import axios from 'axios';
-import { useState, ChangeEvent  } from 'react';
+import Button from '@mui/material/Button';
 
 const App = () => {
-    const [value, setValue] = useState('');
-    const [response, setResponse] = useState('');
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value);
-    }
-
-    const handleClick = async () => {
-        try {
-            const {data} = await axios.get('http://localhost:3000/approutes', {
-                headers: {
-                    'Authorization': `Bearer ${value}`
-                }
-            });
-            setResponse(JSON.stringify(data, null, 2));
-        } catch (err) {
-            setResponse(JSON.stringify(err, null, 2));          
-        }
-    }
-
     return (
         <div>
-            <input type='text' value={value} onChange={(e) => handleChange(e)}></input>
-            <button onClick={handleClick}>test</button>
-
-            <h4>Response:</h4>
-            {response
-                ? <pre>{response}</pre>
-                : null
-            }
+            <Button variant="contained">Hello World</Button>
         </div>
     );
 };
